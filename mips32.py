b741638e593b1d99a8b1c9bc7f8b5702cd5f58e0
John Trammell <johntrammell@gmail.com>
2013-04-05 15:45:34 -0500

    adding library

---
+++ mips32.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    for line in sys.stdin:
+        print line
+
+# parse out operator
+# parse out operands
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+setup(name='mips32',
+      version='0.1',
+      py_modules=['mips32'],
+      description='A rudimentary MIPS32 assembler',
+      author='John Trammell',
+      author_email='johntrammell+pypi@gmail.com')
